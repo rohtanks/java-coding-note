51b37f7c2ec4aeb92da2cdf553807e940c89b6bd
arroh <arroh@12cm.co.kr>
2022-06-15 12:16:23 +0900

    fix: Programmers 핸드폰 번호 가리기 변경 방식 수정
    뒷 4자리 제외한 문자열 *로 마스킹 처리 기능
    - 정규표현식 전방탐색 방식으로 변경

--- src/main/java/PhoneNumberMasking.java
+++ src/main/java/PhoneNumberMasking.java
@@ -1,13 +1,10 @@
 public class PhoneNumberMasking {
     public String solution(String phone_number) {
-        char[] numberArray = phone_number.toCharArray();
-        if (numberArray.length <= 4)
+        if (phone_number.length() <= 4)
             return phone_number;
 
-        for (int i = (numberArray.length - 1) - 4; i >= 0; i--) {
-            numberArray[i] = '*';
-        }
-
-        return new String(numberArray);
+        // 정규표현식 전방탐색으로 구현 방식 변경
+        // 뒤에서 4자리를 제외한 임의의 문자 한개를 *로 변경
+        return phone_number.replaceAll(".(?=.{4})", "*");
     }
 }
